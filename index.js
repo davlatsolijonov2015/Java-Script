da18873f14b22d8e941906515adecbe2a1756add
Davlat Solijonov <solijonovdavlat39@gmail.com>
2024-05-06 16:08:14 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let age = prompt('Yoshingizdi Kiriting')
+
+let name = prompt('Ismingizni Kiriting')
+
+let surname = prompt('Familiyangizni kiriting')
+
+let school = prompt('Sizning makatabingiz qaysi ?')
+
+let хобби = prompt('Hibbyingizni Kiriting')
+
+let LoveBook = prompt('Yoqtirgan kitobingizni Kiriting')
+
+let Film = prompt('Yoqtirgan filmingizni kiriting')
+
+let et = prompt('Yoqtirgan ovqat')
+
+
+
+console.log(' Sizning Yoshingiz ' + age + ' Sizning Ismingiz ' + name + ' Sizning Familyangaiz ' + surname + ' Sizning maktabingiz ' + school + ' Sizning Hobbiyingiz ' + хобби + ' Yoqtirgan kitobingiz ' + LoveBook + ' Yoqtirgan Film ' + Film + ' Yoqtirgan Ovatingiz ' + et)
